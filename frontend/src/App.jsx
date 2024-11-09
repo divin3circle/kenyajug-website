e4559f988024098d8d34004f844eb8ff75acc16d
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-11-09 20:28:55 +0300

    Initialize React project with Vite and Tailwind CSS setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-red-300 w-full">
+      <h1 className="text-bold text-4xl text-blue-700">Hello sylus</h1>
+    </div>
+  );
+}
+
+export default App;
