4e5fca87d0110be97c786fddddba48545df3526a
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-11-10 08:57:35 +0300

    Enhance Landing page with updated styles, new heading, and description to promote Kenya JUG

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import java from "../../public/java.png";
+import { FaJava } from "react-icons/fa";
 
 function Landing() {
   return (
     <div>
       <section
-        className="relative h-[75vh] bg-cover bg-center"
+        className="relative h-[85vh] md:h-[75vh] bg-cover bg-center"
         style={{ backgroundImage: `url(${java})` }}
       >
         <div className="relative z-10">
           <Navbar />
         </div>
-        <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+        <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
+        <h1 className="relative text-white top-1/4 text-6xl font-bold text-center mt-20 font-body">
+          Kenya JUG
+          <FaJava size={64} className="mb-8 inline-block" />
+        </h1>
+        <p className="relative text-white top-1/4 text-2xl text-center font-body">
+          Join us as we connect, share, and grow the Java community in Kenya
+        </p>
       </section>
     </div>
   );
