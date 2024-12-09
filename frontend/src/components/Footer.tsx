fca18bafaf74d964b3d29639db7065299d4049ba
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-09 19:35:57 +0300

    Enhance Footer component styling for better responsiveness and readability

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,9 +4,11 @@ import { FaTwitter, FaWhatsapp, FaYoutube } from "react-icons/fa";
 function Footer() {
   return (
     <div className="bg-black text-gray-300 w-full mt-8 flex items-center sm:justify-between flex-col sm:flex-row justify-center h-[70px] px-4">
-      <div className="flex items-center gap-1 font-body">
-        <p>KENYAJUG - </p>
-        <p>© 2021 All rights reserved</p>
+      <div className="flex items-center gap-1 font-body ">
+        <p className="text-sm md:text-base font-body">KENYAJUG - </p>
+        <p className="text-sm md:text-base font-body">
+          © 2021 All rights reserved
+        </p>
       </div>
       <div className="flex items-center gap-4 justify-center mt-4 sm:mt-0">
         <a href="#">
